cb192fdaa0f2054948a8a761d294f6af82a3faee
cfishbein <croog24@gmail.com>
2019-02-17 17:37:31 -0500

    Add bishop moves

--- src/main/java/chess/game/piece/Bishop.java
+++ src/main/java/chess/game/piece/Bishop.java
@@ -1,7 +1,10 @@
 package chess.game.piece;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import chess.game.Board;
+import chess.game.BoundsCheck;
 import chess.game.Coordinate;
 import chess.game.Movable;
 
@@ -24,8 +27,38 @@ public class Bishop extends Piece implements Movable {
      * @return a {@code Set} of all possible moves
      */
     public static Set<Coordinate> getUnvalidatedBishopMoves(final int row, final int col) {
+        final Set<Coordinate> moves = new HashSet<>();
 
-        return null;
+        for (int i = 1; i < Board.DIMENSION; i++) {
+            final int rowRight = row + i;
+            final int rowLeft = row - i;
+            final int colUp = col + i;
+            final int colDown = col - i;
+
+            if (BoundsCheck.withinBoard(rowRight, colUp)) {
+                moves.add(new Coordinate(rowRight, colUp));
+            }
+
+            if (BoundsCheck.withinBoard(rowLeft, colUp)) {
+                moves.add(new Coordinate(rowLeft, colUp));
+            }
+
+            if (BoundsCheck.withinBoard(rowRight, colDown)) {
+                moves.add(new Coordinate(rowRight, colDown));
+            }
+
+            if (BoundsCheck.withinBoard(rowLeft, colDown)) {
+                moves.add(new Coordinate(rowLeft, colDown));
+            }
+        }
+
+        return moves;
+    }
+
+    public static void main(final String[] args) {
+        for (final Coordinate c : Bishop.getUnvalidatedBishopMoves(4, 2)) {
+            System.out.println(c);
+        }
     }
 
 }
