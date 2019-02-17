bb16c6893112e67e4dbe0fed009a7747b2e8281e
cfishbein <croog24@gmail.com>
2019-02-17 16:54:41 -0500

    King moves

--- src/main/java/chess/game/piece/King.java
+++ src/main/java/chess/game/piece/King.java
@@ -13,8 +13,18 @@ public class King extends Piece implements Movable {
 
     @Override
     public Set<Coordinate> getUnvalidatedMoves() {
-        // TODO Auto-generated method stub
-        return null;
+        final int currRow = currPos.getRow();
+        final int currCol = currPos.getColumn();
+
+        final Coordinate up = new Coordinate(currRow + 1, currCol);
+        final Coordinate down = new Coordinate(currRow - 1, currCol);
+        final Coordinate left = new Coordinate(currRow, currCol - 1);
+        final Coordinate right = new Coordinate(currRow, currCol + 1);
+        final Coordinate upLeft = new Coordinate(currRow + 1, currCol - 1);
+        final Coordinate upRight = new Coordinate(currRow + 1, currCol + 1);
+        final Coordinate downLeft = new Coordinate(currRow - 1, currCol - 1);
+        final Coordinate downRight = new Coordinate(currRow - 1, currCol + 1);
+        return Set.of(up, down, left, right, upLeft, upRight, downLeft, downRight);
     }
 
 }
